dc607188091a1df3e236673026b87b195017529f
Your Name <you@example.com>
2026-04-01 09:03:54 +0530

    render added

--- client/src/api/bookingApi.js
+++ client/src/api/bookingApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/bookings",
+  baseURL: "https://virasathotel-server.onrender.com/api/bookings",
 });
 
 export const createBooking = (data) => API.post("/create", data);
--- client/src/components/BookingForm.jsx
+++ client/src/components/BookingForm.jsx
@@ -58,7 +58,7 @@ const BookingForm = () => {
 
       // 💰 STEP 1: Create Razorpay order
       const { data: order } = await axios.post(
-        "http://localhost:5000/api/bookings/create-order",
+        "https://virasathotel-server.onrender.com/api/bookings/create-order",
         { amount: advance }
       );
 
